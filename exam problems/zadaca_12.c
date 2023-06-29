562f7098d80503ec2f6393b627ba270d528b09f8
Stefan Tagarski <116569281+stefantagarski@users.noreply.github.com>
2023-06-29 15:49:47 +0200

    Update zadaca_12.c

--- exam problems/zadaca_12.c
+++ exam problems/zadaca_12.c
@@ -1,1 +1,37 @@
+//Да се напише рекурзивна функција која ќе го најде бројот на позитивни броеви од целобројна низа.
+// Функцијата како аргумент ја прима низата, за која се бара бројот на позитивни броеви и вкупниот број на елементи, кои ги има таа низа.
+// Функцијата е зададена со следниот прототип: int BrojPozitivni(int niza[], int n);
+//Да се напише и функција main() за тестирање на функцијата BrojPozitivni.
 
+#include <stdio.h>
+int BrojPozitivni (int niza[], int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        if (niza[n - 1] > 0)
+        {
+            return  1 + BrojPozitivni(niza, n - 1);
+        }
+
+        else
+        {
+            return BrojPozitivni(niza, n - 1);
+        }
+    }
+}
+int main() {
+    int n,i;
+    scanf("%d",&n);
+    int array[100];
+    for(i=0;i<n;i++) {
+
+        scanf("%d", &array[i]);
+
+    }
+    printf("%d", BrojPozitivni(array,n));
+    return 0;
+}
